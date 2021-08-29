a37b31ebbd24cf92ab3152acb6bf6e6e8cd6de27
benscruton <benjamin.scruton@gmail.com>
2021-08-28 21:39:24 -0500

    Added new file for promotion pieces

--- client/src/components/Game/GameTypes/standardChess/index.js
+++ client/src/components/Game/GameTypes/standardChess/index.js
@@ -1,9 +1,4 @@
 module.exports = {
   clickUtils: require("./clickUtils"),
-  promotionPieces: [
-    {type: "knight", abbrev: "N"},
-    {type: "bishop", abbrev: "B"},
-    {type: "rook", abbrev: "R"},
-    {type: "queen", abbrev: "Q"}
-  ]
+  promotionPieces: require("./promotionPieces")
 };
---
+++ client/src/components/Game/GameTypes/standardChess/promotionPieces.js
@@ -0,0 +1,6 @@
+module.exports = [
+  {type: "knight", abbrev: "N"},
+  {type: "bishop", abbrev: "B"},
+  {type: "rook", abbrev: "R"},
+  {type: "queen", abbrev: "Q"}
+];
